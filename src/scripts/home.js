cf226d20de2dd78dd6ee80de6a29f8b260fe0044
Paulo-Devz <pv23778@gmail.com>
2026-03-17 08:55:03 -0300

    Feedbacks

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -36,6 +36,54 @@
             //     photo: "src/imgs/imgs++/tioPaulo.png",
             //     feedback: "Very good Esse Site!"
             // }
+            {
+                name: "Paulo",
+                role: "DONO",
+                photo: "src/imgs/imgs++/tioPaulo.png",
+                feedback: "Seu feedback é importante. Mande um email falando como está sendo utilizar o site!"
+            },
+            {
+                name: "Ana Souza",
+                role: "VISITANTE",
+                photo: "src/imgs/imgs++/feedback_persons/feed1.jpg",
+                feedback: "Gostei muito da plataforma, usei várias vezes e sempre funcionou rápido. Bem simples de usar"
+            },
+            {
+                name: "Lucas Martins",
+                role: "VISITANTE",
+                photo: "src/imgs/imgs++/feedback_persons/feed2.jpg",
+                feedback: "Muito bom, resolveu exatamente o que eu precisava. Interface limpa e fácil de entender.”"
+            },
+            {
+                name: "Rafael Oliveira",
+                role: "VISITANTE",
+                photo: "src/imgs/imgs++/feedback_persons/feed3.jpg",
+                feedback: "Usei em um projeto e me ajudou bastante. Recomendo pra quem quer algo rápido e eficiente."
+            },
+            {
+                name: "João Pedro Silva",
+                role: "VISITANTE",
+                photo: "src/imgs/imgs++/feedback_persons/feed4.jpg",
+                feedback: "Curti bastante, simples mas funciona muito bem. Já salvei aqui pra usar sempre."
+            },
+            {
+                name: "Matheus Fernandes",
+                role: "VISITANTE",
+                photo: "src/imgs/imgs++/feedback_persons/feed5.jpg",
+                feedback: "Achei muito útil, principalmente pela praticidade. Vale a pena usar."
+            },
+            {
+                name: "Felipe Andrade",
+                role: "VISITANTE",
+                photo: "src/imgs/imgs++/feedback_persons/feed6.jpg",
+                feedback: "Usei aqui e funcionou certinho, bem direto ao ponto. Curti porque não tem complicação."
+            },
+            {
+                name: "Paulo",
+                role: "DONO",
+                photo: "src/imgs/imgs++/tioPaulo.png",
+                feedback: "Seu feedback é importante. Mande um email falando como está sendo utilizar o site!"
+            },
         ];
 
         // Populate carousel
